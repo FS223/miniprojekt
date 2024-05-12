1284b216356c1a917db9d679c7619af9526053b0
SoftwareSnowStorm <gamefreame@pm.me>
2024-05-12 19:38:51 +0200

    styling dashboard

--- Fitnessstudio/DB.cs
+++ Fitnessstudio/DB.cs
@@ -1,50 +1,53 @@
-using System;
-using Npgsql;
-using System.Data;
+//using System;
+//using Npgsql;
+//using System.Data;
 
-public static class DB
-{
-    private readonly string _connectionString = $"Host={host};Username={user};Password={pass};Database={database}";
+//public static class DB
+//{
+//    private readonly string _connectionString = $"Host={host};Username={user};Password={pass};Database={database}";
 
-    private async DataTable ExecuteQuery(string query)
-    {
-        using (var connection = new NpgsqlConnection(_connectionString))
-        {
-            connection.Open();
-            using (var command = new NpgsqlCommand(query, connection))
-            {
-                var dataTable = new DataTable();
-                var dataAdapter = new NpgsqlDataAdapter(command);
-                dataAdapter.Fill(dataTable);
-                return dataTable;
-            }
-        }
-    }
+//    private async DataTable ExecuteQuery(string query)
+//    {
+//        using (var connection = new NpgsqlConnection(_connectionString))
+//        {
+//            connection.Open();
+//            using (var command = new NpgsqlCommand(query, connection))
+//            {
+//                var dataTable = new DataTable();
+//                var dataAdapter = new NpgsqlDataAdapter(command);
+//                dataAdapter.Fill(dataTable);
+//                return dataTable;
+//            }
+//        }
+//    }
 
-    // ============== Datenbankfunktionen ==============
+//    // ============== Datenbankfunktionen ==============
 
-    public async DataTable getData(String query) { return await ExecuteQuery(query); }
+//    public async DataTable getData(String query) { return await ExecuteQuery(query); }
 
-    public async void setData(String query) { await ExecuteQuery(query); }
+//    public async void setData(String query) { await ExecuteQuery(query); }
 
-    public async void addData(String query) { await ExecuteQuery(query); }
+//    public async void addData(String query) { await ExecuteQuery(query); }
 
-    // ============== Kundenverwaltung ==============
+//    // ============== Kundenverwaltung ==============
 
-    public async DataTable getKunden() {
-        return await getData("SELECT * FROM kunde");
-    }
+//    public async DataTable getKunden()
+//    {
+//        return await getData("SELECT * FROM kunde");
+//    }
 
-    // ==============  Kursverwaltung  ==============
+//    // ==============  Kursverwaltung  ==============
 
-    public async DataTable getKurse() {
-        return await getData("SELECT * FROM kurs");
-    }
+//    public async DataTable getKurse()
+//    {
+//        return await getData("SELECT * FROM kurs");
+//    }
 
-    // ==============   Datenanalyse   ==============
-    
-    public async DataTable getDaten() {
-        return await getData("SELECT * FROM daten");
-    }
-    
-}
+//    // ==============   Datenanalyse   ==============
+
+//    public async DataTable getDaten()
+//    {
+//        return await getData("SELECT * FROM daten");
+//    }
+
+//}
