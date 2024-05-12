9b8e4a1cbe885dfd14dfbadd5cceed2c7195415e
Jonas Pieschel <jonas.pieschel@eviden.com>
2024-05-12 18:59:34 +0200

    dashboard ui design

---
+++ Fitnessstudio/Views/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Fitnessstudio.Views;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fitnessstudio
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+        }
+
+        private void ButtonHome_Click(object sender, RoutedEventArgs e) => FrameWithinGrid.Source = new Uri("Pages/Home.xaml", UriKind.Relative);     
+        private void ButtonStudio_Click(object sender, RoutedEventArgs e) => FrameWithinGrid.Source = new Uri("Pages/Studiosicht.xaml", UriKind.Relative);
+        private void ButtonKunden_Click(object sender, RoutedEventArgs e) => FrameWithinGrid.Source = new Uri("Pages/Kunden.xaml", UriKind.Relative);
+        private void ButtonKurse_Click(object sender, RoutedEventArgs e) => FrameWithinGrid.Source = new Uri("Pages/Kurse.xaml", UriKind.Relative);
+    }
+}
--- Fitnessstudio/Views/Pages/Kunden.xaml.cs
+++ Fitnessstudio/Views/Pages/Kunden.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Fitnessstudio
+namespace Fitnessstudio.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Kunden.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Kunden : Page
     {
-        public MainWindow()
+        public Kunden()
         {
             InitializeComponent();
         }
